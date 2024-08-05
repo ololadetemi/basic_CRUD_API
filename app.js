3405dfb89fbd8c112a5d3fd54cf0c2504547601b
ololadetemi <ololadetoluwalase@gmail.com>
2024-08-05 15:55:51 +0100

    basic nodejs server with db connection

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+require('dotenv').config();
+
+const dbURI = process.env.MONGODB_URI;
+
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true})
+  .then((result) => console.log('Connected!'))
+  .catch((err) => console.log('error connecting to database:', err));
+
+
 
 app.get('/', function (req, res) {
   res.send('Hello World')
@@ -10,6 +20,7 @@ app.get('/blog', function (req, res) {
     res.send('Hello blog world')
   })
 
-app.listen(3030, () => {
-    console.log('server is running on port 3030');
+  const  port = process.env.PORT;
+app.listen(port, () => {
+    console.log(`server is running on port ${port}`);
 });
